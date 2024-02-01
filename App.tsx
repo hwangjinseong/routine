1cba0e2ed3d12429c1b8f3c315fe0535c73ddc06
hwangjinseong <imhjs2023@gmail.com>
2024-02-01 16:18:30 +0900

    statusbar delete

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {PortalHost, PortalProvider} from '@gorhom/portal';
-import {StatusBar} from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import Navigation from './Navigation';
@@ -9,7 +8,6 @@ function App() {
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <SafeAreaProvider>
-        <StatusBar barStyle="dark-content" />
         <PortalProvider>
           <Navigation />
           <PortalHost name="select.modal" />
