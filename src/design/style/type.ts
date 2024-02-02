6b57e5423dde51563cdb9003ef096f7ac3197083
hwangjinseong <imhjs2023@gmail.com>
2024-02-02 15:17:33 +0900

    feat : style animated value type

--- src/design/style/type.ts
+++ src/design/style/type.ts
@@ -1,5 +1,13 @@
-import {StyleProp, ViewStyle} from 'react-native';
+import {Animated, StyleProp, ViewStyle} from 'react-native';
 
 export type PickStyleProp<T extends keyof ViewStyle> = Pick<ViewStyle, T>;
 
 export type PickStyle<T extends keyof ViewStyle> = StyleProp<PickStyleProp<T>>;
+
+export type PickStylePropWithAnimatedValue<T extends keyof ViewStyle> = {
+  [K in T]: ViewStyle[K] | Animated.Value;
+};
+
+export type PickStyleWithAnimatedValue<T extends keyof ViewStyle> = StyleProp<
+  PickStylePropWithAnimatedValue<T>
+>;
