15d6be0b0d89f9280c63e208faf398096391003e
hwangjinseong <imhjs2023@gmail.com>
2024-02-02 14:07:33 +0900

    feat : Row layout

---
+++ src/design/layout/Row.tsx
@@ -0,0 +1,10 @@
+import {ComponentProps} from 'react';
+import Flex from './Flex';
+
+interface Props extends Omit<ComponentProps<typeof Flex>, 'direction'> {}
+
+function Row(props: Props) {
+  return <Flex direction="row" {...props} />;
+}
+
+export default Row;
