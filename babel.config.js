75b06fe4ffbe7d9dd92c6f9975c8b56d98cbb918
hwangjinseong <imhjs2023@gmail.com>
2024-02-27 16:40:26 +0900

    feat : babel.config 설정

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,26 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '~': './src',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
